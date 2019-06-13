41ec1f53dddf58a2c9f84d1663c5338c8d6be006
Kari <kj7rosebud@gmail.com>
2019-06-12 23:25:26 -0400

    Initial commit

---
+++ TravelPlaces/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TravelPlaces
+//
+//  Created by Karen Weis on 6/12/19.
+//  Copyright © 2019 Karen Weis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
